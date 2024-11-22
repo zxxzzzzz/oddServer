3f484e06f6032ef3ce9c2abdf853e614d0f11969
zhangxin <812603872@qq.com>
2024-11-22 19:36:27 +0800

    feat: 同步post

---
+++ src/http/sync.ts
@@ -0,0 +1,10 @@
+import { resolve } from 'path';
+import { server } from './server.js';
+import { writeFileSync } from 'fs';
+import { GlobalFootballState } from 'src/store/football.js';
+
+server.post('/sync/footballState', async (req, res)=>{
+  const body = req.body
+  GlobalFootballState.HGInfoList = body?.HGInfoList || []
+  GlobalFootballState.JCInfoList = body?.JCInfoList || []
+})
